3310cd55f8b19459dbd2e28cb1df9549d923b175
jimis.drpc@gmail.com <jimis.drpc@gmail.com>
2020-11-14 21:17:03 -0300

    fixed issue with endpoint extending wrong class

--- src/main/kotlin/com/mybank/endpoint/TransactionsEndpoint.kt
+++ src/main/kotlin/com/mybank/endpoint/TransactionsEndpoint.kt
@@ -4,24 +4,11 @@ import io.grpc.stub.StreamObserver
 import javax.inject.Singleton
 
 @Singleton
-class TransactionsEndpoint : TransactionsServiceGrpc.TransactionsServiceImplBase(){
+class TransactionsEndpoint : TransactionsServiceGrpcKt.TransactionsServiceCoroutineImplBase(){
 
-    //First tentative
-    //This complains "'postTransactions' overrides nothing" and IntelliJ suggest second next approach
-    //override fun postTransactions(request: TransactionsRequest?) : TransactionsReply {
+    override suspend fun postTransactions(request: TransactionsRequest): TransactionsReply {
+        return TransactionsReply.newBuilder().setMessage("testReply").build()
+    }
 
-    //Second Tentative
-//    override fun postTransactions(request: TransactionsRequest?, responseObserver: StreamObserver<TransactionsReply>?) {
-//        //it complains Type mismatch... Found: TransactionsReply
-//        return TransactionsReply.newBuilder().setMessage("teste").build()
-//    }
-
-    //Third Tentative
-    //This causes:
-    //Return type is 'TransactionsReply', which is not a subtype of overridden public open
-    // fun postTransactions(request: TransactionsRequest!, responseObserver: StreamObserver<TransactionsReply!>!):
-    // Unit defined in com.mybank.endpoint.TransactionsServiceGrpc.TransactionsServiceImplBase
-    //override fun postTransactions(request: TransactionsRequest?, responseObserver: StreamObserver<TransactionsReply>?) : TransactionsReply {
-//        return TransactionsReply.newBuilder().setMessage("teste").build()
-//    }
 }
+
